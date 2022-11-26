a78f609aa081e3d64b40287b29f7dd235761eef1
pavel71r <117903705+pavel71r@users.noreply.github.com>
2022-11-26 13:27:47 +0300

    Add files via upload

---
+++ script1.5.js
@@ -0,0 +1,35 @@
+let btn = document.querySelector(".btn");
+let swiperWrapper = document.querySelector(".swiper-wrapper");
+let btnExpand = document.querySelector(".btn__expand");
+let btnTextOn = document.querySelector(".btn__text-on");
+let btnTextOff = document.querySelector(".btn__text-off");
+
+btn.onclick = function () {
+  swiperWrapper.classList.toggle("swiper-wrapper-open");
+  btnExpand.classList.toggle("btn__expand-deg");
+  btnTextOn.classList.toggle("btn-on");
+  btnTextOff.classList.toggle("btn-off");
+};
+
+let swiper;
+
+let width = window.matchMedia("(max-width: 767px)");
+
+function resize(event) {
+  event == true
+    ? (swiper = new Swiper(".swiper", {
+        slidesPerView: "auto",
+        spaceBetween: 16,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      }))
+    : swiper.destroy();
+}
+width.addEventListener("change", function (event) {
+  resize(event.matches);
+});
+
+resize(width.matches);
